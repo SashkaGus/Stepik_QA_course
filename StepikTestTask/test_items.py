b16d9fb589097e44d2e5c992f8383675c2cea9b7
SashkaGus <a.a.gusev9866@gmail.com>
2022-09-15 11:24:45 +0300

    function name edit

--- StepikTestTask/test_items.py
+++ StepikTestTask/test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_basket_button(browser):
     browser.get(link)
     assert True == browser.find_element(By.CSS_SELECTOR, "button.btn-lg").is_enabled(), "Button is unavailable"
 
