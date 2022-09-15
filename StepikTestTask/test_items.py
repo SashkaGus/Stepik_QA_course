4342cea3a9e3b3cc4b1f2e5fe62fd7f915754cc2
SashkaGus <a.a.gusev9866@gmail.com>
2022-09-15 11:23:02 +0300

    edits by assert

--- StepikTestTask/test_items.py
+++ StepikTestTask/test_items.py
@@ -5,4 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "button.btn-lg")
+    assert True == browser.find_element(By.CSS_SELECTOR, "button.btn-lg").is_enabled(), "Button is unavailable"
+
