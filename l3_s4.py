f729bb56a3d6013f16082e09572ca1849aaba153
SashkaGus <a.a.gusev9866@gmail.com>
2022-09-15 09:45:10 +0300

    conftest and test_l3_s4 files added

--- l3_s4.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver.get("https://stepik.org/lesson/25969/step/8")
